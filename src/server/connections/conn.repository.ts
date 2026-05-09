ec0ec1f8e20a62e082bb4d630ec76392265aa959
Mahboob Hasan <hasanmahboob059@gmail.com>
2026-05-09 23:52:40 +0530

    feat: update to getUserPair

--- src/server/connections/conn.repository.ts
+++ src/server/connections/conn.repository.ts
@@ -1,67 +1,72 @@
 import { database } from "../../db/utils";
 import type { FriendRow, RequestStatus } from "../../shared/types";
 import { now } from "../units/timeUtils";
+import { getUserPair } from "../units/userPair";
+import { createFriendQuery, deleteFriendQuery, deleteFriendRequestQuery, processRequestQuery, searchFriendQuery, searchFriendRequestQuery, searchUserQuery, sendRequestQuery } from "./conn.query";
 import type { FriendsSchema, ProcessFriendRequestSchema, SendFriendRequestSchema } from "./conn.schema";
 type Users = {
   id: number;
   username: string;
 };
-export const searchUser = (query: string): Users[] | undefined => {
-  const q = query;
-
+export const searchUser = (q: string): Users[] | undefined => {
   const result = database
-    .prepare(
-      `SELECT id, username  FROM users WHERE email_id = ? OR mobile_no = ? OR username = ? OR first_name = ? OR last_name = ?;`,
-    )
+    .prepare(searchUserQuery)
     .all(q, q, q, q, q) as Users[] | undefined;
   return result;
 };
 
 export const sendRequest = (sender_id: number, query: SendFriendRequestSchema): number => {
   const result = database.
-    prepare(`INSERT into friend_requests (status, sender_id, receiver_id) VALUES(?, ?, ?)`)
-    .run("pending", sender_id, query.receiver_id);
+    prepare(sendRequestQuery)
+    .run(sender_id, query.receiver_id);
   return result.lastInsertRowid as number;
 };
 
 export const findPendingRequest = (sender_id: number, query: ProcessFriendRequestSchema) => {
+  const { u1, u2 } = getUserPair(sender_id, query.receiver_id);
   const existing = database
-    .prepare(`SELECT status FROM friend_requests WHERE sender_id = ? AND receiver_id = ?`)
-    .get(sender_id, query.receiver_id) as { status: RequestStatus } | null;
+    .prepare(searchFriendRequestQuery)
+    .get(u1, u2) as { status: RequestStatus } | null;
   return existing;
 
 }
 
 export const processRequest = (sender_id: number, query: ProcessFriendRequestSchema): number => {
+  const { u1, u2 } = getUserPair(sender_id, query.receiver_id);
   const result = database.
-    prepare(`UPDATE friend_requests set status = ?, updated_at = ? where sender_id = ? and receiver_id = ?;`)
-    .run(query.status ?? "pending", now(), sender_id, query.receiver_id);
+    prepare(processRequestQuery)
+    .run(query.status, now(),query.status,  u1, u2);
   return result.changes;
 };
 
 export const createFriends = (sender_id: number, query: ProcessFriendRequestSchema): number => {
-  const user1_id = Math.min(sender_id, query.receiver_id);
-  const user2_id = Math.max(sender_id, query.receiver_id);
+  const { u1, u2 } = getUserPair(sender_id, query.receiver_id);
   const result = database.
-    prepare(`INSERT into friends (user1_id, user2_id) VALUES(?, ?)`)
-    .run(user1_id, user2_id);
+    prepare(createFriendQuery)
+    .run(u1, u2);
   return result.lastInsertRowid as number;
 };
 
 export const findFriends = (sender_id: number, query: FriendsSchema): boolean => {
-  const user1_id = Math.min(sender_id, query.user2_id);
-  const user2_id = Math.max(sender_id, query.user2_id);
+  const { u1, u2 } = getUserPair(sender_id, query.user2_id);
   const result = database.
-    prepare(`SELECT 1 FROM friends WHERE user1_id = ? AND user2_id = ? AND deleted = 0;`)
-    .get(user1_id, user2_id) as FriendRow | null;
+    prepare(searchFriendQuery)
+    .get(u1, u2) as FriendRow | null;
   return !!result;
 }
 
 export const deleteFriends = (sender_id: number, query: FriendsSchema): number => {
-  const user1_id = Math.min(sender_id, query.user2_id);
-  const user2_id = Math.max(sender_id, query.user2_id);
+  const { u1, u2 } = getUserPair(sender_id, query.user2_id);
   const result = database.
-    prepare(`UPDATE friends set deleted = 1, updated_at=? WHERE user1_id = ? AND user2_id =?;`)
-    .run(now(), user1_id, user2_id);
+    prepare(deleteFriendQuery)
+    .run(now(), u1, u2);
+  return result.changes;
+};
+
+export const deleteFriendRequest = (sender_id: number, query: FriendsSchema): number => {
+  const { u1, u2 } = getUserPair(sender_id, query.user2_id);
+  const result = database.
+    prepare(deleteFriendRequestQuery)
+    .run(now(), u1, u2);
   return result.changes;
 };
